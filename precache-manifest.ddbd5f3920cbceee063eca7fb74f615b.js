e9c4ec8177f28a998c8ff379170c464083538857
tantrojan <tg.99ghosh@gmail.com>
2020-04-22 19:47:00 +0530

    Updates

---
+++ precache-manifest.ddbd5f3920cbceee063eca7fb74f615b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082cfe0560e9f8bc6da6c3d659cc7a5f",
+    "url": "/Insta_Stat/index.html"
+  },
+  {
+    "revision": "981e5b5a4ac7c19be9d8",
+    "url": "/Insta_Stat/static/js/2.b9950e41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Insta_Stat/static/js/2.b9950e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85b07cee6183041654c",
+    "url": "/Insta_Stat/static/js/main.f9d182ed.chunk.js"
+  },
+  {
+    "revision": "b0eaa7ccf13232f3de5c",
+    "url": "/Insta_Stat/static/js/runtime-main.82c0bc2a.js"
+  }
+]);
